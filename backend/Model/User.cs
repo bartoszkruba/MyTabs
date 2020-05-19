6a9c585a571df51403f60ad4a30dc0f6de2a7de9
bartoszkruba <bartosz.kruba@gmail.com>
2020-05-19 17:22:21 +0200

    EntityFramework setup + User table + migration

---
+++ backend/Model/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Model
+{
+    public class User
+    {
+        [Key] public int Id { get; set; }
+
+        [Required, MinLength(4), MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required, EmailAddress] public string Email { get; set; }
+
+        [Required] public string Password { get; set; }
+    }
+}
